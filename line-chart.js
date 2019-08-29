b0e2d31c5af37845dcd4bc289c27c54b400d921c
Luka Triska <triska@ucu.edu.ua>
2019-08-29 18:07:32 -0400

    add volume line to chart

--- line-chart.js
+++ line-chart.js
@@ -1,6 +1,9 @@
-let margin = {top: 10, right: 50, bottom: 100, left: 50},
+let margin = {top: 10, right: 90, bottom: 100, left: 50},
   width = 760,
   height = 400;
+
+const getDateString = (date) => `${date.getDate()} ${date.toLocaleString('default', {month: 'long'})} ${date.getFullYear()}`;
+
 
 const svg = d3.select(".main-container")
   .append("svg")
@@ -9,30 +12,61 @@ const svg = d3.select(".main-container")
   .attr("height", height + margin.top + margin.bottom)
   .style("cursor", "crosshair")
   .append("g")
-  .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
 
 d3.csv("https://api.coindesk.com/v1/bpi/historical/close.csv",
 
   d => {
-    return {date: d3.timeParse("%Y-%m-%d")(d.Date), value: Math.floor(d.Close)}
+    return {date: d3.timeParse("%Y-%m-%d")(d.Date), value: Math.floor(d.Close), volume: Math.floor(d.volume)}
   },
+
 
   data => {
 
+
+    let m_cap_url = "https://min-api.cryptocompare.com/data/exchange/histoday?tsym=USD&api_key=86cf65ef09473df5964e859710cbccbb0c12284cd4e5493023133a9f5035429f";
+
+    // getting the market cap data
+    let xhReq = new XMLHttpRequest();
+    xhReq.open("GET", m_cap_url, false);
+    xhReq.send(null);
+    let m_cap_data = JSON.parse(xhReq.responseText)["Data"];
+
     data.splice(data.length - 3, 3);
 
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < m_cap_data.length; j++) {
+        let m_cap_data_date = new Date(new Date(m_cap_data[j]["time"] * 1e3).toISOString());
 
+        if (getDateString(data[i].date) === getDateString(m_cap_data_date)) data[i].volume = m_cap_data[j]["volume"];
+
+      }
+    }
+    console.log(data);
+
+
+    const showMostRecentInfo = () => {
+      d3.select(".chart-info__date")
+        .text("");
+      d3.select(".chart-info__price")
+        .text(`Most recent price: $${data[data.length - 1].value}`);
+    };
+
+    showMostRecentInfo();
+
+    // appending the bottom x axis with dates
     let x = d3.scaleTime()
       .domain(d3.extent(data, d => d.date))
       .range([0, width]);
     svg.append("g")
-      .attr("transform", 'translate(0,' + height + ')')
+      .attr("transform", `translate(0,${height})`)
       .call(d3.axisBottom(x))
       .classed('axis', true)
       .selectAll("text")
       .attr("transform", "translate(-10,10)rotate(-45)");
 
+    // appending the left y axis with price
     let y = d3.scaleLinear()
       .domain([0, d3.max(data, d => +d.value)])
       .range([height, 0]);
@@ -40,6 +74,18 @@ d3.csv("https://api.coindesk.com/v1/bpi/historical/close.csv",
       .classed('axis', true)
       .call(d3.axisLeft(y));
 
+    // appending the right y axis with volume
+    let y_volume = d3.scaleLinear()
+      .domain([0, d3.max(data, d => {
+        return +d.volume;
+      })])
+      .range([height, 0]);
+    svg.append("g")
+      .attr("transform", `translate(${width},0)`)
+      .classed('axis-right', true)
+      .call(d3.axisRight(y_volume));
+
+    // appending the price line to the graph
     svg.append("path")
       .datum(data)
       .attr("fill", "none")
@@ -48,6 +94,17 @@ d3.csv("https://api.coindesk.com/v1/bpi/historical/close.csv",
       .attr("d", d3.line()
         .x(d => x(d.date))
         .y(d => y(d.value))
+      );
+
+    // appending the volume line to the graph
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "red")
+      .attr("stroke-width", 3)
+      .attr("d", d3.line()
+        .x(d => x(d.date))
+        .y(d => y_volume(d.volume))
       );
 
     const focus = svg.append('g')
@@ -78,7 +135,10 @@ d3.csv("https://api.coindesk.com/v1/bpi/historical/close.csv",
       .attr('width', width)
       .attr('height', height)
       .on('mouseover', () => focus.style('display', null))
-      .on('mouseout', () => focus.style('display', 'none'))
+      .on('mouseout', () => {
+        showMostRecentInfo();
+        return focus.style('display', 'none');
+      })
       .on('mousemove', mouseAction());
 
     function mouseAction() {
@@ -98,8 +158,6 @@ d3.csv("https://api.coindesk.com/v1/bpi/historical/close.csv",
       return value;
     }
 
-    console.log(data);
-
     function updateMousePosition(target) {
       let mouse = d3.mouse(target);
 
@@ -117,7 +175,7 @@ d3.csv("https://api.coindesk.com/v1/bpi/historical/close.csv",
         d3.select(".chart-info__date")
           .text(`Date: ${d.date.getDate()} ${d.date.toLocaleString('default', {month: 'long'})} ${d.date.getFullYear()}`);
         d3.select(".chart-info__price")
-          .text(`Price: $${d.value}`)
+          .text(`Price: $${d.value}, $${d.volume}`)
       }
 
     }
